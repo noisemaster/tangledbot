d8c6f3a1f10ca8e95cb00f6985986d729e8b5bf7
noisemaster <psp1001k2006@gmail.com>
2016-11-22 13:09:48 -0500

    Reformated reddit embed

--- main.go
+++ main.go
@@ -233,7 +233,6 @@ func onReady(s *discordgo.Session, event *discordgo.Ready) {
 }
 
 func sendRedditPost(s *discordgo.Session, m *discordgo.MessageCreate, sub string) {
-	var e discordgo.MessageEmbed
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", "https://www.reddit.com/r/"+sub+".json", nil)
 	if err != nil {
@@ -256,10 +255,12 @@ func sendRedditPost(s *discordgo.Session, m *discordgo.MessageCreate, sub string
 	json.Unmarshal(body, &info)
 	for _, v := range info.Data.Children {
 		if !v.Data.Announcement {
-			e.Title = "Here's the latest post from r/" + sub
-			e.URL = "https://www.reddit.com/r/" + sub
-			e.Color = 0xE5343A
-			e.Description = "[" + v.Data.Title + "](" + v.Data.URL + ")\n"
+			var e = discordgo.MessageEmbed{
+				Title:       "Here's the latest post from r/" + sub,
+				URL:         "https://www.reddit.com/r/" + sub,
+				Color:       0xE5343A,
+				Description: "[" + v.Data.Title + "](" + v.Data.URL + ")\n",
+			}
 			if strings.Contains(v.Data.URL, ".jpg") || strings.Contains(v.Data.URL, ".png") || strings.Contains(v.Data.URL, ".gif") || strings.Contains(v.Data.Domain, "imgur") {
 				e.Image = &discordgo.MessageEmbedImage{URL: v.Data.URL}
 			} else if v.Data.SelfPost {
