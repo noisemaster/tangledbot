10207a8381e537626cadb5278e70a7082733c537
Christian Harrypersad <psp1001k2006@gmail.com>
2020-01-19 16:38:17 -0500

    Show live game scores

--- commands/sport/nfl_commands.go
+++ commands/sport/nfl_commands.go
@@ -19,6 +19,7 @@ type nflScoresResult struct {
 
 type nflSchedule struct {
 	ScheduledGame nflScheduleEntry `json:"gameSchedule"`
+	Score         nflScore         `json:"score,omitempty"`
 }
 
 type nflScheduleEntry struct {
@@ -33,6 +34,32 @@ type nflScheduleEntry struct {
 	VisitorTeamAbbr    string `json:"visitorTeamAbbr"`
 	HomeDisplayName    string `json:"homeDisplayName"`
 	VisitorDisplayName string `json:"visitorDisplayName"`
+}
+
+type nflScore struct {
+	Time               string              `json:"time,omitempty"`
+	Down               int                 `json:"down,omitempty"`
+	YardsToGo          int                 `json:"yardsToGo,omitempty"`
+	Yardline           string              `json:"yardline,omitempty"`
+	YardlineSide       string              `json:"yardlineSide,omitempty"`
+	YardlineNumber     int                 `json:"yardlineNumber,omitempty"`
+	Phase              string              `json:"phase,omitempty"`
+	PhaseDescription   string              `json:"phaseDescription,omitempty"`
+	PossessionTeamID   string              `json:"possessionTeamID,omitempty"`
+	PossessionTeamAbbr string              `json:"possessionTeamAbbr,omitempty"`
+	RedZone            bool                `json:"redZone,omitempty"`
+	VisitorTeamScore   nflTeamQuarterScore `json:"visitorTeamScore,omitempty"`
+	HomeTeamScore      nflTeamQuarterScore `json:"homeTeamScore,omitempty"`
+}
+
+type nflTeamQuarterScore struct {
+	PointTotal        int `json:"pointTotal,omitempty"`
+	PointQ1           int `json:"pointQ1,omitempty"`
+	PointQ2           int `json:"pointQ2,omitempty"`
+	PointQ3           int `json:"pointQ3,omitempty"`
+	PointQ4           int `json:"pointQ4,omitempty"`
+	PointOT           int `json:"pointOT,omitempty"`
+	TimeoutsRemaining int `json:"timeoutsRemaining,omitempty"`
 }
 
 // FindNFLGames shows the active games for the current week of the NFL
@@ -64,10 +91,28 @@ func FindNFLGames(s *discordgo.Session, m *discordgo.MessageCreate) {
 	for _, game := range results.GameScores {
 		gameTime := time.Unix(game.ScheduledGame.IsoTime/1000, 0)
 		year, month, day := gameTime.Date()
+		currentTime := time.Now()
+
+		var scoreString string
+		timeString := month.String() + " " + strconv.Itoa(day) + ", " + strconv.Itoa(year) + " " + gameTime.Format(time.Kitchen) + " (Eastern)"
+		var finalValue string
+
+		if currentTime.Unix() > gameTime.Unix() {
+			scoreString = game.ScheduledGame.VisitorTeamAbbr + " " + strconv.Itoa(game.Score.VisitorTeamScore.PointTotal) + " - " + game.ScheduledGame.HomeTeamAbbr + " " + strconv.Itoa(game.Score.HomeTeamScore.PointTotal)
+			if game.Score.Phase != "FINAL" {
+				timeString = "<:live:668567946997792800> LIVE:"
+			}
+		}
+
+		if scoreString != "" {
+			finalValue = timeString + " " + game.Score.PhaseDescription + " ||" + scoreString + "||"
+		} else {
+			finalValue = timeString
+		}
 
 		gameField := discordgo.MessageEmbedField{
 			Name:   game.ScheduledGame.VisitorDisplayName + " @ " + game.ScheduledGame.HomeDisplayName,
-			Value:  month.String() + " " + strconv.Itoa(day) + ", " + strconv.Itoa(year) + " " + gameTime.Format(time.Kitchen) + " (Eastern)",
+			Value:  finalValue,
 			Inline: false,
 		}
 		fields = append(fields, &gameField)
