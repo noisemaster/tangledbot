628373f76d2882344cc1ddf052094e3fe150267e
lot <psp1001k2006@gmail.com>
2021-02-05 00:32:58 -0500

    Change endpoint, use embed for quote, add more information

--- commands/stock.ts
+++ commands/stock.ts
@@ -1,18 +1,19 @@
 import { Interaction } from "https://deno.land/x/harmony@v1.0.0/src/structures/slash.ts";
 import { InteractionResponseType } from "https://deno.land/x/harmony@v1.0.0/src/types/slash.ts";
 import { format } from "https://deno.land/x/date_fns@v2.15.0/index.js";
+import { Embed } from "https://deno.land/x/harmony@v1.0.0/src/structures/embed.ts";
 
 export const fetchQuote = async (interaction: Interaction) => {
     const symbolOption = interaction.data.options.find(option => option.name === 'symbol');
     const symbol: string = symbolOption ? symbolOption.value : '';
 
-    const url = `https://query1.finance.yahoo.com/v8/finance/chart/${symbol}`;
+    const url = `https://query1.finance.yahoo.com/v7/finance/quote?symbols=${symbol}`
 
     const stockResp = await fetch(url);
     const stock = await stockResp.json();
-    const {result} = stock.chart;
+    const {result} = stock.quoteResponse;
 
-    if (!result) {
+    if (result.length === 0) {
         await interaction.respond({
             type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
             content: `${symbol} not found`
@@ -20,13 +21,22 @@ export const fetchQuote = async (interaction: Interaction) => {
         return;
     }
 
-    const [{meta: data}] = result;
-    const {symbol: returnedSymbol, regularMarketPrice, regularMarketTime} = data;
+    const [data] = result;
+    const {symbol: returnedSymbol, longName, regularMarketPrice, regularMarketChange, regularMarketChangePercent, regularMarketTime} = data;
     const lastRefresh = new Date(regularMarketTime * 1000);
-    const lastRefreshFriendly = format(lastRefresh, 'h:mm a z', undefined);
-    
+    const lastRefreshFormat = format(lastRefresh, "yyyy-MM-dd'T'HH:mm:ssxxx", undefined);
+    const diffSymbol = regularMarketChange > 0 ? '🔺' : '🔻';
+    const diffColor = regularMarketChange > 0 ? 0x44bd32 : 0xe74c3c;
+
+    const stockEmbed = new Embed({
+        title: `${longName} (${returnedSymbol})`,
+        description: `${regularMarketPrice.toFixed(2)}\n${diffSymbol} ${Math.abs(regularMarketChange).toFixed(2)} (${regularMarketChangePercent.toFixed(2)}%)`,
+        timestamp: lastRefreshFormat,
+        color: diffColor
+    });
+
     await interaction.respond({
         type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
-        content: `${returnedSymbol} as of ${lastRefreshFriendly}: ${regularMarketPrice}`
+        embeds: [stockEmbed]
     });
 }
