a5c9c849b9c60855f47ffe46a23353487b7006df
Christian Harrypersad <psp1001k2006@gmail.com>
2021-03-29 23:16:33 -0400

    Fix logging with no options

--- commands/lib/log.ts
+++ commands/lib/log.ts
@@ -2,8 +2,8 @@ import { InteractionData } from "https://deno.land/x/harmony@v1.1.4/mod.ts";
 import { green, brightRed, gray, bold } from "https://deno.land/std@0.90.0/fmt/colors.ts"
 
 export const logInteraction = ({id, name, options}: InteractionData) => {
-    console.log(
-`[${gray(id)}] Command: ${bold(green(name))}
-${options.map(x => `${green(x.name)}: ${brightRed(x.value)}`).join(`\n`)}`
-    )
+    console.log(`[${gray(id)}] Command: ${bold(green(name))}`);
+    if (options) {
+        console.log(`${options.map(x => `${green(x.name)}: ${brightRed(x.value)}`).join(`\n`)}`);
+    }
 }
