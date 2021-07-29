c168c3a62079608111dc6725b1b9d487f2c3edd5
Christian Harrypersad <psp1001k2006@gmail.com>
2021-07-28 23:29:20 -0400

    Add timerange selects to stock commands

--- commands/stock.ts
+++ commands/stock.ts
@@ -1,8 +1,36 @@
-import { Embed, InteractionResponseType, SlashCommandInteraction } from 'https://deno.land/x/harmony@v2.0.0-rc2/mod.ts'
+import { Embed, InteractionResponseType, MessageComponentInteraction, SlashCommandInteraction } from 'https://deno.land/x/harmony@v2.0.0-rc2/mod.ts'
 import { format } from "https://deno.land/x/date_fns@v2.15.0/index.js";
 import { MessageAttachment } from "https://deno.land/x/harmony@v2.0.0-rc2/mod.ts";
-import { AllWebhookMessageOptions } from "https://deno.land/x/harmony@v2.0.0-rc2/src/structures/webhook.ts";
-import { sendInteraction } from "./lib/sendInteraction.ts";
+import { webhookOptionsWithAttachments } from '../interfaces/webhookOptionsWithAttachments.ts';
+import { generateTimerangeButtons, HasTimerange, setTimerangePost, timerangePost } from "../handlers/timerangeHandler.ts";
+import { v4 } from "https://deno.land/std@0.97.0/uuid/mod.ts";
+
+interface YahooStockQuote extends HasTimerange {
+    symbol: string,
+    exchange: string,
+    quoteType: string,
+    coinImageUrl: string,
+    fromCurrency: string,
+    longName: string,
+    shortName: string,
+    regularMarketPrice: number,
+    regularMarketChange: number,
+    regularMarketChangePercent: number,
+    regularMarketTime: number
+}
+
+const YahooTimeranges = [
+    { name: "1 Day", value: "1d" },
+    { name: "5 Days", value: "5d" },
+    { name: "1 Month", value: "1mo" },
+    { name: "3 Months", value: "3mo" },
+    { name: "6 Months", value: "6mo" },
+    { name: "1 Year", value: "1y" },
+    { name: "2 Years", value: "2y" },
+    { name: "5 Years", value: "5y" },
+    { name: "Year to Date", value: "ytd" },
+    { name: "Max", value: "max" },
+]
 
 export const fetchQuote = async (interaction: SlashCommandInteraction) => {
     if (!interaction.data) {
@@ -36,25 +64,54 @@ export const fetchQuote = async (interaction: SlashCommandInteraction) => {
         }
     }
 
-    const [data] = result;
-    const {symbol: returnedSymbol, exchange, quoteType, coinImageUrl, fromCurrency, longName, shortName, regularMarketPrice, regularMarketChange, regularMarketChangePercent, regularMarketTime} = data;
+    const [data]: [YahooStockQuote] = result;
 
-    if (!regularMarketTime) {
+    if (!data.regularMarketTime) {
         await interaction.send(`${symbol} not found`, {});
         return;
     }
+
+    const payload = await generateStockEmbed(data, timeRange)
+
+    const timerangeData: timerangePost<YahooStockQuote> = {
+        data,
+        currentTime: timeRange,
+        embedMessage: payload.embeds![0],
+        interactionData: {
+            timeRange,
+        },
+        poster: interaction.user.id,
+        timeRanges: YahooTimeranges,
+        timeRangeHandler: stockTimerangeHandler
+    };
+
+    const internalMessageId = v4.generate();
+    const timerangeComponents = generateTimerangeButtons('stock', timerangeData, internalMessageId)
+
+    await interaction.send({
+        ...payload,
+        components: [
+            ...timerangeComponents
+        ]
+    });
+
+    setTimerangePost(internalMessageId, timerangeData);
+}
+
+const generateStockEmbed = async (quoteData: YahooStockQuote, timerange: string) => {
+    const {symbol, exchange, quoteType, coinImageUrl, fromCurrency, longName, shortName, regularMarketPrice, regularMarketChange, regularMarketChangePercent, regularMarketTime} = quoteData;
 
     const lastRefresh = new Date(regularMarketTime * 1000);
     const lastRefreshFormat = format(lastRefresh, "yyyy-MM-dd'T'HH:mm:ssxxx", undefined);
     const diffSymbol = regularMarketChange > 0 ? '<:small_green_triangle:851144859103395861>' : '🔻';
     const diffColor = regularMarketChange > 0 ? 0x44bd32 : 0xe74c3c;
     
-    const image = await fetchChart(returnedSymbol, timeRange).catch(err => {
+    const image = await fetchChart(symbol, timerange).catch(err => {
         console.log(err);
     });
     
     const stockEmbed = new Embed({
-        title: `${longName || shortName} (${returnedSymbol})`,
+        title: `${longName || shortName} (${symbol})`,
         timestamp: lastRefreshFormat,
         color: diffColor,
     });
@@ -66,24 +123,54 @@ export const fetchQuote = async (interaction: SlashCommandInteraction) => {
         });
         stockEmbed.setDescription(`${regularMarketPrice}\n${diffSymbol} ${Math.abs(regularMarketChange)} (${regularMarketChangePercent.toFixed(2)}%)`);
     } else {
-        stockEmbed.setDescription(`${regularMarketPrice.toFixed(2)}\n${diffSymbol} ${Math.abs(regularMarketChange.toFixed(2))} (${regularMarketChangePercent.toFixed(2)}%)`);
+        stockEmbed.setDescription(`${regularMarketPrice.toFixed(2)}\n${diffSymbol} ${Math.abs(regularMarketChange).toFixed(2)} (${regularMarketChangePercent.toFixed(2)}%)`);
         stockEmbed.setFooter(`Exchange: ${exchange}`);
     }
 
-    const payload: AllWebhookMessageOptions = {
+    const payload: webhookOptionsWithAttachments = {
         embeds: [stockEmbed],
-    };
+        attachments: [],
+    }
 
     if (image) {
-        const imageAttach = new MessageAttachment(`${returnedSymbol}.png`, image);
+        const imageAttach = new MessageAttachment(`${symbol}.png`, image);
         payload.file = imageAttach;
 
         stockEmbed.setImage({
-            url: `attachment://${returnedSymbol}.png`
+            url: `attachment://${symbol}.png`
         });
     }
 
-    await interaction.send(payload);
+    return payload;
+}
+
+const stockTimerangeHandler = async (interaction: MessageComponentInteraction, pageData: timerangePost<YahooStockQuote>) => {
+    const {custom_id: customId} = interaction.data;
+    const [_componentId, _commandInvoker, _action, messageId] = customId.split('_');
+
+    console.log(customId);
+
+    const [timerange] = (interaction.data as any).values;
+    pageData.currentTime = timerange;
+
+    const newEmbed = await generateStockEmbed(pageData.data, pageData.currentTime);
+    const timerangeComponents = generateTimerangeButtons('stock', pageData, messageId);
+
+    if (interaction.message) {
+        await interaction.editMessage(
+            interaction.message, {
+                ...newEmbed,
+                allowed_mentions: {
+                    users: []
+                },
+                components: [
+                    ...timerangeComponents
+                ],
+            }
+        );
+    }
+
+    setTimerangePost(messageId, pageData);
 }
 
 const fetchChart = async (symbol: string, timeRange: string): Promise<Uint8Array> => {
