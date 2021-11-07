af614f63554029a3c640bcf1670b2c72c0f71bdc
Christian Harrypersad <psp1001k2006@gmail.com>
2021-11-07 17:13:19 -0500

    Allow for mode selection in command update script

--- bin/update_command.ts
+++ bin/update_command.ts
@@ -1,8 +1,14 @@
 import config from '../config.ts';
 import {NFLCommandSchema as commandRequest} from '../commands/schemas/nfl.ts';
 
-const url = `https://discord.com/api/v8/applications/${config.discord.appID}/commands`;
-// const url = `https://discord.com/api/v8/applications/${config.discord.appID}/guilds/${config.discord.testingGuildID}/commands`;
+const [mode = 'prod'] = Deno.args;
+let url = '';
+
+if (mode === 'prod') {
+    url = `https://discord.com/api/v8/applications/${config.discord.appID}/commands`;
+} else if (mode === 'test') {
+    url = `https://discord.com/api/v8/applications/${config.discord.appID}/guilds/${config.discord.testingGuildID}/commands`;
+}
 
 const headers = {
     "Authorization": `Bot ${config.discord.token}`,
