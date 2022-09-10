e6f418f7a675e19c739de83afe8592e3fc78f989
Christian Harrypersad <psp1001k2006@gmail.com>
2022-09-09 22:15:32 -0400

    Start rebuild with discordeno

--- main.ts
+++ main.ts
@@ -1,105 +1,112 @@
-import { Client, event, Intents, InteractionType, MessageComponentInteraction, slash, SlashCommandInteraction, subslash } from 'https://deno.land/x/harmony@v2.6.0/mod.ts'
-import { handleTeamAutocomplete, sendNFLEmbed, sendNFLGameDetails } from "./commands/nfl.ts";
-import { sendRedditEmbed } from "./commands/reddit.ts";
-import { togglePost } from "./handlers/imagePostHandler.ts";
 import config from './config.ts';
-import { sendE621Embed } from "./commands/booru.ts";
-import { sendValorantFixtureEmbed } from "./commands/valorant.ts";
-import { generateIsThisImage } from "./commands/image/isthis.ts";
-import { sendShowEmbed } from "./commands/frinkiac.ts";
-import { fetchQuote } from "./commands/stock.ts";
-import { fetchMovie } from "./commands/tmdb.ts";
-import { sendCryptoEmbed } from "./commands/crypto.ts";
-import { GlobalCommandSchemas } from './commands/schemas/index.ts';
-import { updatePage } from "./handlers/paginationHandler.ts";
-import { updateTimerange } from "./handlers/timerangeHandler.ts";
 
-class TangledClient extends Client {
-    @event() ready() {
-        console.log(`Ready - ${client.user?.tag}`);
-    }
+import { createBot, GatewayIntents } from 'discordeno/mod.ts';
 
-    @subslash('nfl', 'this-week') async nflThisWeek(interaction: SlashCommandInteraction) {
-        await sendNFLEmbed(interaction)
-            .catch(err => console.log(err));
-    }
+const bot = createBot({
+    token: config.discord.token,
+    intents: GatewayIntents.Guilds
+});
 
-    @subslash('nfl', 'details') async nflGameDetails(interaction: SlashCommandInteraction) {
-        await sendNFLGameDetails(interaction)
-            .catch(err => console.log(err));
-    }
 
-    @slash() async reddit(interaction: SlashCommandInteraction) {
-        await sendRedditEmbed(interaction)
-            .catch(err => console.log(err));
-    }
 
-    @slash() async e621(interaction: SlashCommandInteraction) {
-        await sendE621Embed(interaction)
-            .catch(err => console.log(err));
-    }
+// class TangledClient extends Client {
+//     constructor() {
+//         super({
+//             token: config.discord.token,
+//             intents: Intents.NonPrivileged, 
+//         })
+//     }
 
-    @slash() async valorant(interaction: SlashCommandInteraction) {
-        await sendValorantFixtureEmbed(interaction)
-            .catch(err => console.log(err));
-    }
+//     @event() ready() {
+//         console.log(`Ready - ${client.user?.tag}`);
+//     }
 
-    @slash() async isthis(interaction: SlashCommandInteraction) {
-        await generateIsThisImage(interaction)
-            .catch(err => console.log(err));
-    }
+//     @event() error(...args: any[]) {
+//         console.log('Events');
+//         console.log(args);
+//     }
 
-    @slash() async show(interaction: SlashCommandInteraction) {
-        await sendShowEmbed(interaction)
-            .catch(err => console.log(err));
-    }
+//     @subslash('nfl', 'this-week') async nflThisWeek(interaction: SlashCommandInteraction) {
+//         await sendNFLEmbed(interaction)
+//             .catch(err => console.log(err));
+//     }
 
-    @slash() async stock(interaction: SlashCommandInteraction) {
-        await fetchQuote(interaction)
-            .catch(err => console.log(err));
-    }
+//     @subslash('nfl', 'details') async nflGameDetails(interaction: SlashCommandInteraction) {
+//         await sendNFLGameDetails(interaction)
+//             .catch(err => console.log(err));
+//     }
 
-    @slash() async movie(interaction: SlashCommandInteraction) {
-        await fetchMovie(interaction)
-            .catch(err => console.log(err));
-    }
+//     @slash() async reddit(interaction: SlashCommandInteraction) {
+//         await sendRedditEmbed(interaction)
+//             .catch(err => console.log(err));
+//     }
 
-    @slash() async crypto(interaction: SlashCommandInteraction) {
-        await sendCryptoEmbed(interaction)
-            .catch(err => console.log(err));
-    }
+//     @slash() async e621(interaction: SlashCommandInteraction) {
+//         await sendE621Embed(interaction)
+//             .catch(err => console.log(err));
+//     }
 
-    @event() async interactionCreate(interaction: MessageComponentInteraction) {
-        try {
-            if (interaction.type === InteractionType.MESSAGE_COMPONENT && interaction.data) {
-                const { custom_id: customId } = interaction.data;
+//     @slash() async valorant(interaction: SlashCommandInteraction) {
+//         await sendValorantFixtureEmbed(interaction)
+//             .catch(err => console.log(err));
+//     }
+
+//     @slash() async isthis(interaction: SlashCommandInteraction) {
+//         await generateIsThisImage(interaction)
+//             .catch(err => console.log(err));
+//     }
+
+//     @slash() async show(interaction: SlashCommandInteraction) {
+//         await sendShowEmbed(interaction)
+//             .catch(err => console.log(err));
+//     }
+
+//     @slash() async stock(interaction: SlashCommandInteraction) {
+//         await fetchQuote(interaction)
+//             .catch(err => console.log(err));
+//     }
+
+//     @slash() async movie(interaction: SlashCommandInteraction) {
+//         await fetchMovie(interaction)
+//             .catch(err => console.log(err));
+//     }
+
+//     @slash() async crypto(interaction: SlashCommandInteraction) {
+//         await sendCryptoEmbed(interaction)
+//             .catch(err => console.log(err));
+//     }
+
+//     @event() async interactionCreate(interaction: MessageComponentInteraction) {
+//         try {
+//             if (interaction.type === InteractionType.MESSAGE_COMPONENT && interaction.data) {
+//                 const { custom_id: customId } = interaction.data;
         
-                    if (customId.startsWith('hideable_')) {
-                        await togglePost(interaction);
-                    }
+//                     if (customId.startsWith('hideable_')) {
+//                         await togglePost(interaction);
+//                     }
                     
-                    if (customId.startsWith('pageable_')) {
-                        await updatePage(interaction);
-                    }
+//                     if (customId.startsWith('pageable_')) {
+//                         await updatePage(interaction);
+//                     }
         
-                    if (customId.startsWith('timerange_')) {
-                        await updateTimerange(interaction);
-                    }
-            //@ts-ignore Autocomplete Interaction Type
-            } else if (interaction.type === 4) {
-                const interactionData: any = interaction.data;
+//                     if (customId.startsWith('timerange_')) {
+//                         await updateTimerange(interaction);
+//                     }
+//             //@ts-ignore Autocomplete Interaction Type
+//             } else if (interaction.type === 4) {
+//                 const interactionData: any = interaction.data;
 
-                if (interactionData.name === 'nfl') {
-                    await handleTeamAutocomplete(interaction);
-                }
-            }
-        } catch (err) {
-            // console.log(interaction);
-            console.log(err);
-        }
-    }
-}
+//                 if (interactionData.name === 'nfl') {
+//                     await handleTeamAutocomplete(interaction);
+//                 }
+//             }
+//         } catch (err) {
+//             // console.log(interaction);
+//             console.log(err);
+//         }
+//     }
+// }
 
-const client = new TangledClient();
+// const client = new TangledClient();
 
-client.connect(config.discord.token, Intents.NonPrivileged);
+// client.connect();
