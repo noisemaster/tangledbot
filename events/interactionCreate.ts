7a539969c5e6fabc5142deb0b35ca77dc580b892
Christian Harrypersad <psp1001k2006@gmail.com>
2022-09-11 22:02:33 -0400

    Fix typo in option type check

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -3,11 +3,8 @@ import { Command, commands } from "../commands/mod.ts";
 import { events } from './mod.ts';
 
 events.interactionCreate = (bot: Bot, interaction: Interaction) => {
-    console.log(interaction);
-
     if (interaction.data && interaction.id) {
         let command: undefined | Command = interaction.data.name ? commands.get(interaction.data.name) : undefined;
-        let commandName = command?.name;
         
         if (!command) {
             return;
@@ -25,45 +22,58 @@ events.interactionCreate = (bot: Bot, interaction: Interaction) => {
 
         if (optionType === ApplicationCommandOptionTypes.SubCommandGroup) {
           // Check if command has subcommand and handle types
-          if (!command.subcommands) return;
+          if (!command.subcommands) {
+            return;
+          }
 
           // Try to find the subcommand group
           const subCommandGroup = command.subcommands?.find(
             (command) => command.name == interaction.data?.options?.[0].name,
           );
-          if (!subCommandGroup) return;
+          console.log(subCommandGroup)
 
-          if (!hasProperty(subCommandGroup, "subCommands")) return;
+          if (!subCommandGroup) {
+            return;
+          }
+
+          if (!hasProperty(subCommandGroup, "subCommands")) {
+            return;
+          }
 
           // Get name of the command which we are looking for
           const targetCmdName = interaction.data.options?.[0].options?.[0].name ||
             interaction.data.options?.[0].options?.[0].name;
-          if (!targetCmdName) return;
+
+          if (!targetCmdName) {
+            return;
+          }
 
           // Try to find the command
           command = subCommandGroup.subCommands.find((c) => c.name === targetCmdName);
-
-          commandName += ` ${subCommandGroup.name} ${command?.name}`;
-
-          // Normal
         }
 
-        if (optionType === ApplicationCommandOptionTypes.SubCommandGroup) {
+        if (optionType === ApplicationCommandOptionTypes.SubCommand) {
           // Check if command has subcommand and handle types
-          if (!command?.subcommands) return;
+          if (!command?.subcommands) {
+            return;
+          }
 
           // Try to find the command
           const found = command.subcommands.find((command) => command.name == interaction.data?.options?.[0].name);
-          if (!found) return;
+          if (!found) {
+            return;
+          }
 
-          if (hasProperty(found, "subCommands")) return;
+          if (hasProperty(found, "subCommands")) {
+            return;
+          }
 
           command = found;
-          commandName += ` ${command?.name}`;
         }
 
         try {
             if (command) {
+              console.log(command);
               command.execute(bot, interaction);
             } else {
               throw "";
