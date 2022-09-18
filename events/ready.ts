c944bee4a8bba67f6f37b79b17988a9442ae3845
Christian Harrypersad <psp1001k2006@gmail.com>
2022-09-18 12:22:01 -0400

    Sync commands on load

--- events/ready.ts
+++ events/ready.ts
@@ -1,5 +1,19 @@
+import { commands } from "../commands/mod.ts";
+import config from "../config.ts";
 import { events } from "./mod.ts";
 
-events.ready = (bot) => {
+events.ready = async (bot) => {
     console.log('Ready');
+
+    // console.log(commands.array());
+    // await bot.helpers.upsertGuildApplicationCommands();
+
+    await bot.helpers.upsertGuildApplicationCommands(
+        config.discord.testingGuildID,
+        commands.array()
+    );
+
+    await bot.helpers.upsertGlobalApplicationCommands(
+        commands.array()
+    );
 }
