08b49cb8ac4fd5e319e0d33ce425427d07c9dd51
Christian Harrypersad <psp1001k2006@gmail.com>
2022-09-28 22:40:55 -0400

    Add cron job to get timeseries scoreboard data

---
+++ helpers/yahoo-fantasy/updateScoreboardRedisJob.ts
@@ -0,0 +1,12 @@
+import { addPoints } from "./mod.ts";
+import {cron} from 'https://deno.land/x/deno_cron/cron.ts';
+
+cron('1 */5 19-23 * * 1,4', () => {
+    console.log(new Date(), 'Getting Points');
+    addPoints();
+});
+
+cron('1 */5 9-23 * * 0', () => {
+    console.log(new Date(), 'Getting Points');
+    addPoints();
+});
