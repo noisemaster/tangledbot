c324955546620c6faab2b2d2065da5b3603957ca
lot <psp1001k2006@gmail.com>
2023-08-14 17:16:49 -0400

    remove script import calls

--- helpers/yahoo-fantasy/mod.ts
+++ helpers/yahoo-fantasy/mod.ts
@@ -389,27 +389,27 @@ export const collectTransactions = async () => {
     }
 };
 
-const accessToken = await getAccessToken();
-console.log(accessToken);
+// const accessToken = await getAccessToken();
+// console.log(accessToken);
 
-const {transactions} = await getTransactions(accessToken, '1353821', '414'); 
+// const {transactions} = await getTransactions(accessToken, '1353821', '414'); 
 
-// console.log(transactions);
+// // console.log(transactions);
 
-for (let transaction of transactions) {
-    console.log(transaction)
+// for (let transaction of transactions) {
+//     console.log(transaction)
 
-    if (Array.isArray(transaction.players.player)) {
-        for (let player of transaction.players.player) {
-            const playerDetais = await getPlayerDetails(accessToken, player.player_id, '414');
-            console.log(`Updated ${playerDetais.name.full}`)
-        }
-    } else {
-        const player = await getPlayerDetails(accessToken, transaction.players.player.player_id, '414');
-        console.log(`Updated ${player.name.full}`)
-    }
+//     if (Array.isArray(transaction.players.player)) {
+//         for (let player of transaction.players.player) {
+//             const playerDetais = await getPlayerDetails(accessToken, player.player_id, '414');
+//             console.log(`Updated ${playerDetais.name.full}`)
+//         }
+//     } else {
+//         const player = await getPlayerDetails(accessToken, transaction.players.player.player_id, '414');
+//         console.log(`Updated ${player.name.full}`)
+//     }
 
-    await (new Promise(resolve => setTimeout(resolve, 2000)));
-}
+//     await (new Promise(resolve => setTimeout(resolve, 2000)));
+// }
 
-Deno.exit(0);
+// Deno.exit(0);
